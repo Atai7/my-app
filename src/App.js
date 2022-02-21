41054fc16f9eb38bc3275ebf29bb095fc5c43a5c
Atai <ukievatai5@gmail.com>
2022-02-21 13:12:09 +0600

    Фундамент и стили

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
 import logo from './logo.svg';
 import './App.css';
+import './heroes/heroes';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className='wrapper'>
+      <h2 className="user">User choice is:</h2>
+        <h2 className="comp">Computer choice is:</h2>
+        <button href="#" className="btn" onClick={()=> {console.log('clicked')}}>Rock</button>
+        <button href="#" className="btn" onClick={()=> {console.log('clicked')}}>Paper</button>
+        <button href="#" className="btn" onClick={()=> {console.log('clicked')}}>Scissorc</button>
+      </div>
     </div>
   );
 }
